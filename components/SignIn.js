9b64e394f41fb1876e88f6d0864dfd0c905f7cf0
paulinepasseri <paulinecpasseri@gmail.com>
2025-05-02 17:14:34 +0200

    finished login page

---
+++ components/SignIn.js
@@ -0,0 +1,39 @@
+import styles from "../styles/Modal.module.css";
+import Image from "next/image";
+import { useState } from 'react'
+
+import { useDispatch } from 'react-redux'
+import { login } from '../reducers/user'
+
+function SignIn() {
+    const dispatch = useDispatch()
+    const [ signInUsername, setSignInUsername ] = useState('')
+    const [ signInPassword, setSignInPassword ] = useState('')
+
+    const handleLogin = () => {
+      fetch('http://localhost:3000/users/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: signInUsername, password: signInPassword }),
+      }).then(response => response.json())
+        .then(data => {
+          if (data.result) {
+            dispatch(login({ username: signInUsername, firstname: data.firstname, token: data.token }));
+            setSignInUsername('');
+            setSignInPassword('');
+          }
+        });
+    }
+
+    return(
+      <div className={styles.modalContainer}>
+        <Image src='/logohome.png' alt='Logo Twitter' width={50} height={50}/>
+        <p className={styles.text}>Connect to Hackatweet</p>
+        <input className={styles.input} type="text" placeholder="Username" id="signInUsername" onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername} />
+        <input className={styles.input} type="text" placeholder="Password" id="signInPassword" onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword} />
+        <button className={styles.button} onClick={handleLogin()}>Sign in</button> 
+      </div>
+    )
+}
+
+export default SignIn
