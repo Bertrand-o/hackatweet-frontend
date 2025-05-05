129d1ee7c8636923809507a1a169ba4c0dac588c
Your name <Your email>
2025-05-05 14:47:06 +0200

    error

--- components/LastTweets.js
+++ components/LastTweets.js
@@ -5,6 +5,7 @@ import { faHeart, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { refreshTweets } from "../reducers/tweets";
+import moment from 'moment';
 
 function LastTweets() {
   const dispatch = useDispatch();
@@ -37,8 +38,23 @@ function LastTweets() {
       });
   };
 
-  const tweetsList = tweets.map((data, index) => {
+  const handleLike = (tweetId) => {
+    fetch(`http://localhost:3000/tweets/likes/${tweetId}`, {
+      method: 'PUT',
+      headers: { 'Content-Type' : 'application/json' },
+      body: JSON.stringify({
+        token: user.token
+      })
+    })
+    .then(response => response.json())
+    .then(() => {
+      dispatch(refreshTweets());
+    })
+  }
 
+  const tweetsList = tweets.map((data, index) => {
+    console.log(data.likes)
+    const style = {color: '#E60071'}
     return (
       <div className={styles.tweet} key={index}>
         <div className={styles.userInfo}>
@@ -51,12 +67,12 @@ function LastTweets() {
           />
           <span className={styles.firstname}>{data.author.firstname} </span>
           <span className={styles.username}>@{data.author.username} </span>
-          <span className={styles.date}>• {}</span>
+          <span className={styles.date}>• {moment(data.date).fromNow()}</span>
         </div>
         <div className={styles.text}>{data.text}</div>
         <div className={styles.icons}>
-          <span className={styles.heartIcon}>
-            <FontAwesomeIcon icon={faHeart} /> 0
+          <span className={styles.heartIcon} style={data.likes.find(users => users.token === user.token) ? style : {}}>
+            <FontAwesomeIcon icon={faHeart} onClick={() => handleLike(data._id)}/> {data.likes.length}
           </span>
           {data.author.token === user.token ? (
             <FontAwesomeIcon
