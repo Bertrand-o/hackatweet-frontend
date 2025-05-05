7dbb77b834fee7fb35efefbb9de7c7cea1d84082
paulinepasseri <paulinecpasseri@gmail.com>
2025-05-05 16:34:21 +0200

    added trends

--- components/Trends.js
+++ components/Trends.js
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
 import styles from "../styles/Trends.module.css";
-function Trends(){
+import { useSelector } from 'react-redux'
+import { useRouter } from "next/router"
+
+function Trends() {
+  const router = useRouter()
   const [ trends, setTrends ] = useState([])
+  const [ tweetCount, setTweetCount ] = useState({})
+  const refresh = useSelector((state) => state.tweets.needsRefresh);
 
   useEffect(() => {
     fetch('http://localhost:3000/tweets/trends')
@@ -10,14 +16,41 @@ function Trends(){
       if(data.result) {
         setTrends(data.trends)
       }
+      data.trends.forEach(trend => {
+        handleTweetCount(trend);
+      });
     })
-  }, [])
+  }, [refresh])
 
-  const trendsList = trends.map((data, index) => {
+  const handleTweetCount = (trend) => {
+    const trendToString = trend.toString()
+    const trendSliced = trendToString.slice(1)
+    fetch(`http://localhost:3000/tweets/trends/${trendSliced}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.result) {
+        setTweetCount(prev => ({
+          ...prev,
+          [trend]: data.tweets.length,
+        }));
+      } else {
+        setTweetCount(prev => ({
+          ...prev,
+          [trend]: 0,
+        }));
+      }
+    })
+  }
+
+  const handleRouting = (trend) => {
+    router.push(`/home/[${trend}]`)
+  }
+
+  const trendsList = trends.map((trend, index) => {
     return (
-      <div className={styles.trend}>
-        {data}
-        <p className={styles.trendsNumber}>0 Tweets</p> 
+      <div className={styles.trend} key={index} onClick={() => handleRouting(trend)}>
+        <p className={styles.trendName}>{trend}</p>
+        <p className={styles.trendsNumber}>{tweetCount[trend]} Tweets</p> 
       </div>
     )
   })
